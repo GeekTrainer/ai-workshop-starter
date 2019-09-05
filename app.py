02178f9d1ac911661f6c7b2bc9c4e3206f52e794
Christopher Harrison <chrhar@microsoft.com>
2019-09-05 12:20:14 -0700

    Set new starting location

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+# app.py
+import base64
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Define route for the app's one and only page
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        # Display the uploaded image
+        image = request.files["file"]
+        uri = "data:;base64," + base64.b64encode(image.read()).decode("utf-8")
+
+    else:
+        # Display a placeholder image
+        uri = "/static/placeholder.png"
+
+    return render_template("index.html", image_uri=uri)
