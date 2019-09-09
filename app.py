d84d7a3d0f306d1df4dc4ab96c25110a470a0cae
Christopher Harrison <chrhar@microsoft.com>
2019-09-09 16:29:23 -0700

    In progress for streamline

--- app.py
+++ app.py
@@ -1,18 +1,47 @@
 # app.py
-import base64
+import os, base64
 from flask import Flask, render_template, request
+
+# Load system variables with dotenv
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load endpoint
+
+# Load keys
+
+# Create vision_client
+
+# Create face_client
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    # Load image or placeholder
+    uri = get_image_uri(request)
+    # Create a placeholder for messages
+    messages = []
+
+    # TODO: Add code to retrieve text from picture
+
+    # TODO: Add code to translate text
+
+    return render_template("index.html", image_uri=uri, messages=messages)
+
+@app.route("/train", methods=["GET", "POST"])
+def train():
+    # Load image or placeholder
+    image_uri = get_image_uri(request)
+    # Create a placeholder for messages
+    messages = []
+
+    # TODO: Add code 
+
+def get_image_uri(request):
+    # Set a placeholder if no image is uploaded
+    uri = "/static/placeholder.png"
     if request.method == "POST":
-        # Display the uploaded image
         image = request.files["file"]
         uri = "data:;base64," + base64.b64encode(image.read()).decode("utf-8")
-
-    else:
-        # Display a placeholder image
-        uri = "/static/placeholder.png"
-
-    return render_template("index.html", image_uri=uri)
+    return uri
